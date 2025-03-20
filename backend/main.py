322cc5af6b3a2329f261d99d3accfc2540db2ac5
Henrique Machado <heyhenriquecastro@gmail.com>
2025-03-20 17:51:56 -0300

    add new page for fetching all flashcard in frontend

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI, APIRouter
 from api import flashcards
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 router = APIRouter()
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
 
 app.include_router(flashcards.router, prefix="/api", tags=["api"])
 
